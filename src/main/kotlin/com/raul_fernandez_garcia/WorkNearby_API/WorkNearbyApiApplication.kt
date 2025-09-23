cffde5538293a9e108d61f72381b367b821845ee
jraul <jraulfernandezg@gmail.com>
2025-09-23 20:10:33 +0200

    Commit inicial

---
+++ src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/WorkNearbyApiApplication.kt
@@ -0,0 +1,11 @@
+package com.raul_fernandez_garcia.WorkNearby_API
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkNearbyApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkNearbyApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/raul_fernandez_garcia/WorkNearby_API/WorkNearbyApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.raul_fernandez_garcia.WorkNearby_API
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkNearbyApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
