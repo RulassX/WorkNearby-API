e3e2e256ff4bbc2dc203fb187f26dbc177a29e91
jraul <jraulfernandezg@gmail.com>
2025-11-22 00:42:09 +0100

    cambios

--- src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/modelo/Oferta.kt
+++ src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/modelo/Oferta.kt
@@ -23,8 +23,41 @@ data class Oferta(
     @JoinColumn(name = "categoria_id")
     var categoria: Categoria? = null,
 
-    var foto: String? = null,
+    @Lob
+    @Column(columnDefinition = "MEDIUMBLOB")
+    var foto: ByteArray? = null,
 
     @Column(name = "fecha_publicacion")
     val fechaPublicacion: LocalDateTime = LocalDateTime.now()
-)
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Oferta
+
+        if (id != other.id) return false
+        if (precio != other.precio) return false
+        if (trabajador != other.trabajador) return false
+        if (titulo != other.titulo) return false
+        if (descripcion != other.descripcion) return false
+        if (categoria != other.categoria) return false
+        if (!foto.contentEquals(other.foto)) return false
+        if (fechaPublicacion != other.fechaPublicacion) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + (precio?.hashCode() ?: 0)
+        result = 31 * result + trabajador.hashCode()
+        result = 31 * result + titulo.hashCode()
+        result = 31 * result + descripcion.hashCode()
+        result = 31 * result + (categoria?.hashCode() ?: 0)
+        result = 31 * result + (foto?.contentHashCode() ?: 0)
+        result = 31 * result + fechaPublicacion.hashCode()
+        return result
+    }
+}
--- src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/modelo/Usuario.kt
+++ src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/modelo/Usuario.kt
@@ -17,12 +17,47 @@ data class Usuario(
     val email: String,
     val password: String,
     val telefono: String,
-    @Column(name = "foto_perfil")
-    var fotoPerfil: String? = null,
+
+    @Lob
+    @Column(name = "foto_perfil", columnDefinition = "MEDIUMBLOB")
+    val fotoPerfil: ByteArray? = null,
     val rol: String,
     @Column(name = "fecha_registro")
     val fechaReg: LocalDateTime = LocalDateTime.now()
-)
+
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Usuario
+
+        if (id != other.id) return false
+        if (nombre != other.nombre) return false
+        if (apellidos != other.apellidos) return false
+        if (email != other.email) return false
+        if (password != other.password) return false
+        if (telefono != other.telefono) return false
+        if (!fotoPerfil.contentEquals(other.fotoPerfil)) return false
+        if (rol != other.rol) return false
+        if (fechaReg != other.fechaReg) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + nombre.hashCode()
+        result = 31 * result + apellidos.hashCode()
+        result = 31 * result + email.hashCode()
+        result = 31 * result + password.hashCode()
+        result = 31 * result + telefono.hashCode()
+        result = 31 * result + (fotoPerfil?.contentHashCode() ?: 0)
+        result = 31 * result + rol.hashCode()
+        result = 31 * result + fechaReg.hashCode()
+        return result
+    }
+}
 
 
 
