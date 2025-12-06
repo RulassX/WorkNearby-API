effc8e60d467e5c1263bf7d45318f5f4afe44392
jraul <jraulfernandezg@gmail.com>
2025-12-06 19:55:08 +0100

    cambios

--- src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/service/ClienteService.kt
+++ src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/service/ClienteService.kt
@@ -12,8 +12,8 @@ class ClienteService(
     private val clienteRepository: ClienteRepository
 ) {
     //movil fisico: ip local
-    private val BASE_URL = "http://192.168.1.134:8080"
-    //private val BASE_URL = "http://192.168.0.20:8080"
+    //private val BASE_URL = "http://192.168.1.134:8080"
+    private val BASE_URL = "http://192.168.0.20:8080"
 
     //OBTENER PERFIL
     fun obtenerPerfilPorUsuarioId(idUsuario: Int): ClienteDTO {
