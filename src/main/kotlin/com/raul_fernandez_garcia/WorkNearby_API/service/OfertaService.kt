f23f7240f781c7f19b5d7b1b6f93520d8861f23e
jraul <jraulfernandezg@gmail.com>
2025-12-06 01:40:30 +0100

    cambios

--- src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/service/OfertaService.kt
+++ src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/service/OfertaService.kt
@@ -18,7 +18,7 @@ class OfertaService(
     private val categoriaRepository: CategoriaRepository
 ) {
     //prueba con emulador: 10.0.2.2
-    private val BASE_URL = "http://192.168.1.134:8080"
+    private val BASE_URL = "http://192.168.0.20:8080"
 
     //LISTAR OFERTAS
     fun obtenerOfertasFiltradas(latCliente: Double?, lonCliente: Double?): List<OfertaDTO> {
