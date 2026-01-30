0c00793f01689544dc0ff59762e0266e6dd5ff33
jraul <jraulfernandezg@gmail.com>
2026-01-30 14:00:45 +0100

    cambios

--- src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/service/AuthService.kt
+++ src/main/kotlin/com/raul_fernandez_garcia/WorkNearby_API/service/AuthService.kt
@@ -21,7 +21,8 @@ class AuthService(
     //Emulador: 10.0.2.2
     //Móvil físico: IP local
     //private val BASE_URL = "http://192.168.1.134:8080"
-    private val BASE_URL = "http://192.168.0.20:8080"
+    //private val BASE_URL = "http://192.168.0.20:8080"
+    private val BASE_URL = "http://192.168.0.15:8080"
 
     //esto seria para encriptar la contraseña
    // private val encoder = BCryptPasswordEncoder()
